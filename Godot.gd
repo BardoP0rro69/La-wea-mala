b3eff9c25fb112654bc6e2f9ae7adb83f943b766
BardoP0rro69 <didacblancothau@gmail.com>
2022-03-16 08:56:29 +0100

    noves escenes
    he creat noves escenes

--- Godot.gd
+++ Godot.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends Area2D
 
 
 # Declare member variables here. Examples:
@@ -11,16 +11,24 @@ var direccio = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = Vector2(500, 200)
+	position = Vector2(239, 262)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += direccio * velocitat * delta
-	
+	direccio = Vector2.ZERO
 	if Input.is_action_pressed("mou dreta"):
-		direccio = Vector2(1,0)
+		direccio[0] = 1
+	if Input.is_action_pressed("mou esquerra"):
+		direccio[0] = -1
+	if Input.is_action_pressed("mou abaix"):
+		direccio[1] = 1
+	if Input.is_action_pressed("mou adalt"):
+		direccio[1] = -1
 		
+func _on_Personatges_area_entered(area):
+	$Obama.texture = load ('pez.jpg')
 	
-	
-	
+func _on_Personatges_area_exited(area):
+	$Obama.texture = load ('obama.jpg')
