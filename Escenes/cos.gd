2b4f5d2cbb632b19c1fa663ae9a330d4774316ad
BardoP0rro69 <didacblancothau@gmail.com>
2022-03-23 08:57:37 +0100

    overhaul
    dale a tu cuerpo alegria macarena que tu cuerpo es vidaq para darle cosa buena, dale a tu cuerpo alegria macarena eeeee macarena

--- Escenes/cos.gd
+++ Escenes/cos.gd
@@ -4,23 +4,18 @@ var velocitat_base = 300
 var velocitat = Vector2.DOWN * velocitat_base
 var gravetat = Vector2.DOWN * 980
 var salt = Vector2.UP * 500
-var salts_disponibles = 2
 
 func _physics_process(delta):
 
 	
 	velocitat.x = 0
 	
-	if salts_disponibles <= 0 and is_on_floor():
-		salts_disponibles = 2
-	
 	if Input.is_action_pressed("mou dreta"):
 		velocitat += Vector2.RIGHT * velocitat_base
 	if Input.is_action_pressed("mou esquerra"):
 		velocitat += Vector2.LEFT * velocitat_base
-	if Input.is_action_just_pressed("mou adalt") and salts_disponibles > 0:
+	if Input.is_action_just_pressed("mou adalt") and is_on_floor():
 		velocitat += salt
-		salts_disponibles -= 1
 		
 	if Input.is_action_pressed("correr"):
 		velocitat_base = 500
@@ -28,7 +23,7 @@ func _physics_process(delta):
 		velocitat_base = 300
 		
 	if position.y >= 1000:
-		position = Vector2(500,270)
+		position = Vector2(-173,185)
 
 	velocitat += gravetat * delta
 	velocitat = move_and_slide(velocitat, Vector2.UP) 
@@ -37,10 +32,10 @@ func _physics_process(delta):
 	
 func anima(velocitat: Vector2):
 	var animacio : AnimatedSprite = $AnimatedSprite
-	if velocitat.x < 0:
+	if velocitat.x > 0:
 		animacio.flip_h = false
 		animacio.play('corriendo')
-	elif velocitat.x > 0:
+	elif velocitat.x < 0:
 		animacio.flip_h = true
 		animacio.play('corriendo')
 	
